f6587d9b80cd572daf2dc094f47f66ec567997fd
Ignacio R Espino <irespino@capozios.com>
2018-09-12 18:15:16 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,23 @@
 def alphabetize(arr)
-  # code here
+  index = 0
+  result = []
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  names_alpha = alphabet.index(names_letter[index] ) #=>25
+  p names_letter #=> returns array of first letters
+  p names_letter.sort #=> sorts array alphabetically
+  p sorted_first = names_letter.sort #=> array of sorted first letters
+  p names_alpha #=> gives the index in the alphabet of the names_letter
+  p sorted_first
+
+    
+  sorted_first[0] = names[0]
+  sorted_first.each do |x , i | 
+    x = names.index(x)
+  end
+  p sorted_first
+  names_letter.each_with_index do 
+    |name, i|
+    if name(names_letter[i]) == sorted_first[i]
+      sorted_first[i] = names[i]
+  end
 end
