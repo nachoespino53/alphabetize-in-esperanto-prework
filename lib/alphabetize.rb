19ffa47e86e67d07d88baf39cf76bd7d59cc8089
Ignacio R Espino <irespino@capozios.com>
2018-09-12 18:23:04 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
-  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
- a_to_z_hash = alpha_hash(alphabet) #turns alphabet into hash with values as numbers
-word_hash = make_word_hash(arr) #creates hash from words to be alphabetized.
-word_hash = set_word_hash_values(a_to_z_hash, word_hash) #sets values of word hash to array of #'s'
-words = sort_words(word_hash) #sorts words
+    alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+   a_to_z_hash = alpha_hash(alphabet) #turns alphabet into hash with values as numbers
+  word_hash = make_word_hash(arr) #creates hash from words to be alphabetized.
+  word_hash = set_word_hash_values(a_to_z_hash, word_hash) #sets values of word hash to array of #'s'
+  words = sort_words(word_hash) #sorts words
 end
